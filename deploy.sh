faad940a17466f7b37bb8c78b25d060c64f50a13
Janindu Ranaweera <janindu@janindu.local>
2025-10-03 11:30:21 +0530

    tst

--- deploy.sh
+++ deploy.sh
@@ -3,45 +3,32 @@ set -e
 
 STACK_NAME="my-poc-stack"
 S3_BUCKET="mo-poc-s3"
-PACKAGED_FILE="packaged.yaml"
-TEMPLATE_FILE="main.yaml"
-CHILD_TEMPLATE_FILE="child-template.yaml"
+REGION="us-east-1"
+CHANGE_SET_NAME="changeset-$(date +%s)"
 
-echo "Uploading child template..."
-aws s3 cp $CHILD_TEMPLATE_FILE s3://$S3_BUCKET/
+echo "🚀 Uploading child template to S3..."
+aws s3 cp child-template.yaml s3://$S3_BUCKET/child-template.yaml
 
-CHILD_TEMPLATE_URL="https://s3.amazonaws.com/$S3_BUCKET/$CHILD_TEMPLATE_FILE"
-echo "Child template URL: $CHILD_TEMPLATE_URL"
-
-echo "Packaging parent template..."
+echo "📦 Packaging main template..."
 aws cloudformation package \
-  --template-file $TEMPLATE_FILE \
+  --template-file main.yaml \
   --s3-bucket $S3_BUCKET \
-  --output-template-file $PACKAGED_FILE
+  --output-template-file packaged.yaml
 
-CHANGESET_NAME="changeset-$(date +%s)"
-echo "Creating change set: $CHANGESET_NAME..."
-
-if aws cloudformation describe-stacks --stack-name $STACK_NAME >/dev/null 2>&1; then
-  echo "Stack exists. Using UPDATE change set..."
-  CHANGESET_TYPE=UPDATE
-else
-  echo "Stack does not exist. Using CREATE change set..."
-  CHANGESET_TYPE=CREATE
-fi
-
+echo "🛠️ Creating change set: $CHANGE_SET_NAME..."
 aws cloudformation create-change-set \
   --stack-name $STACK_NAME \
-  --change-set-name $CHANGESET_NAME \
-  --template-body file://$PACKAGED_FILE \
-  --parameters ParameterKey=ChildTemplateS3Url,ParameterValue=$CHILD_TEMPLATE_URL \
-  --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
-  --change-set-type $CHANGESET_TYPE
+  --template-body file://packaged.yaml \
+  --change-set-name $CHANGE_SET_NAME \
+  --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+  --parameters ParameterKey=ChildTemplateS3Url,ParameterValue="https://$S3_BUCKET.s3.amazonaws.com/child-template.yaml" \
+  --region $REGION
 
-echo "Waiting for change set to be created..."
-aws cloudformation wait change-set-create-complete --stack-name $STACK_NAME --change-set-name $CHANGESET_NAME
+echo "⏳ Waiting for change set creation to complete..."
+aws cloudformation wait change-set-create-complete \
+  --stack-name $STACK_NAME \
+  --change-set-name $CHANGE_SET_NAME
 
-echo "Executing change set..."
-aws cloudformation execute-change-set --stack-name $STACK_NAME --change-set-name $CHANGESET_NAME
-
-echo "Deployment complete ✅"
+echo "✅ Change set created successfully. Review it in the AWS CloudFormation console:"
+echo "   - Stack Name: $STACK_NAME"
+echo "   - Change Set Name: $CHANGE_SET_NAME"
